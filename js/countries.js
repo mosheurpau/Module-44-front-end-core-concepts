f2d1dc141113af0bd5ad19de4436f28a1a036b36
mosheurpau <mosheur.pau@gamil.com>
2022-03-15 23:09:49 +0600

    done

--- js/countries.js
+++ js/countries.js
@@ -5,7 +5,43 @@ const loadCountries = () => {
 }
 
 const displayCounttries = countries => {
-    console.log(countries);
-    const allCountriesHTML = countries.map(country => getCountryHTML)
+    // console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    console.log(countriesHTML)
+    document.getElementById('countries').innerHTML = countriesHTML.join(' ');
 }
+
+// orginal
+/*
+const getCountryHTML = country => {
+    return `
+        <div class ="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+} */
+
+// option 2
+const getCountryHTML = ({ name, flags, area, region }) => {
+    return `
+        <div class ="country">
+            <h2>${name.common}</h2>
+            <p>Area: ${area}</p>
+            <p>Region: ${region}</p>
+            <img src="${flags.png}">
+        </div>
+    `
+}
+
+// option 1
+/* const getCountryHTML = country => {
+    const { name, flags } = country;
+    return `
+        <div class ="country">
+            <h2>${name.common}</h2>
+            <img src="${flags.png}">
+        </div>
+    `
+} */
 loadCountries();
